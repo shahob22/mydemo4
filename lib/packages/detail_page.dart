e68ab8d3b0f2dc3451a3fc6a379157661fc5f5eb
shahob22 <shahobtursunovdev22@gmail.com>
2021-05-31 21:21:58 +0500

    Yangilanish!

---
+++ lib/packages/detail_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  static final String id = 'detail_page';
+  const DetailPage({Key key}) : super(key: key);
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
